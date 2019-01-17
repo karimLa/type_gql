5841580b0688a78d337ddea6645b16ce23221ff0
karimLa <kira00luffy@gmail.com>
2019-01-17 11:48:00 -0800

    Register resolver

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,17 @@
 import 'reflect-metadata'
 import { ApolloServer } from 'apollo-server-express'
 import * as express from 'express'
-import { buildSchema, Resolver, Query } from 'type-graphql'
+import { buildSchema } from 'type-graphql'
+import { createConnection } from 'typeorm'
+import { RegisterResolver } from './modules/user/Register'
 
-@Resolver()
-class HelloResolver {
-  @Query(() => String)
-  async hello() {
-    return 'Hello World'
-  }
-}
+
 
 const main = async () => {
+  await createConnection()
+
   const schema = await buildSchema({
-    resolvers: [HelloResolver]
+    resolvers: [RegisterResolver]
   })
 
   const apolloServer = new ApolloServer({ schema })
